eb748e78331566e6e33437315961fd71a32d7fd9
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-01-23 18:40:25 -0500

    git ignore models ⛳

---
+++ APRENDE E INGENIA/CREACION DE DATA SET/project/main.py
@@ -0,0 +1,29 @@
+import torch
+import cv2
+import os
+import numpy as np
+
+current_directory = os.getcwd()
+
+#Leer modelo
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=f'{current_directory}/APRENDE E INGENIA/CREACION DE DATA SET/project/models/best.pt')
+print(model)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    #Lectura de frames
+    ret, frame = cap.read()
+    
+    #realizar deteccciones
+    detect = model(frame)
+    
+    #mostrar fps
+    cv2.imshow('detector de balones', np.squeeze(detect.render()))
+    
+    #cerrar con teclado
+    exit = cv2.waitKey(5)
+    if exit == 27:
+        break
+cap.relase()
+cv2.destroyAllWindows()  #cerrar todas las ventanas
